fc536a52d266a6200e218fe4178acc95d45821f3
MainAero <mainaero@gmx.de>
2020-12-01 17:38:06 +0100

    fix: image quality

--- imagemin.config.js
+++ imagemin.config.js
@@ -1,13 +1,13 @@
 module.exports = {
     "gifsicle": { "optimizationLevel": 2, "interlaced": false, "colors": 10 },
-    "mozjpeg": { "progressive": true, "quality": 10 },
-    "pngquant": { "quality": [0.25, 0.5] },
+    "mozjpeg": { "progressive": true, "quality": 100 },
+    "pngquant": { "quality": [1] },
     "svgo": {
       "plugins": [
         { "removeViewBox": false },
         { "cleanupIDs": true },
       ]
     },
-    "webp": { "quality": 10 }
+    "webp": { "quality": 100 }
   }
    
